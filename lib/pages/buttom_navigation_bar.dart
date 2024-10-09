ec1e96d2d47357149e3c763b1130f1100b397cfd
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-10-09 19:18:32 +0700

    first-commit

---
+++ lib/pages/buttom_navigation_bar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ButtomNavigationBarPage extends StatefulWidget {
+  const ButtomNavigationBarPage({super.key});
+
+  @override
+  State<ButtomNavigationBarPage> createState() =>
+      _ButtomNavigationBarPageState();
+}
+
+class _ButtomNavigationBarPageState extends State<ButtomNavigationBarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const primaryC = Color(0xFF008FA0);
+const primaryC_200 = Color(0x4B0090A0);
+const secondaryC = Color(0xFFF7F7F7);
+const greyC = Color(0xFFAFAFAF);
+const greyC_800 = Color(0xFFA2A2A2);
+const whiteC = Color(0xFFFFFFFF);
+const blackC = Color(0xFF000000);
+const blackC_800 = Color(0xFF1E1E1E);
+const blackC_600 = Color(0xFF3D3D3D);
+const blackC_400 = Color(0xFF585858);
