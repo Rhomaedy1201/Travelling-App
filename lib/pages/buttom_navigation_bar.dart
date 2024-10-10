bf70dba0536a64cb2550ea3f25da25b72b347661
Muhammad Rhomaedi <rhomaedycreator@gmail.com>
2024-10-10 20:34:09 +0700

    main: fix bottomNavigationBar

--- lib/pages/buttom_navigation_bar.dart
+++ lib/pages/buttom_navigation_bar.dart
@@ -15,9 +15,9 @@ class _ButtomNavigationBarPageState extends State<ButtomNavigationBarPage> {
   int myCurrentIndex = 0;
   List<Widget> body = const [
     HomePage(),
-    Icon(Icons.message),
-    Icon(Icons.favorite),
-    Icon(Icons.person),
+    Center(child: Icon(Icons.message)),
+    Center(child: Icon(Icons.favorite)),
+    Center(child: Icon(Icons.person)),
   ];
 
   List itemsButtom = [
@@ -29,94 +29,72 @@ class _ButtomNavigationBarPageState extends State<ButtomNavigationBarPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        top: false,
-        child: Stack(
-          children: [
-            Positioned(
-              child: body[myCurrentIndex],
-            ),
-            Positioned(
-              bottom: 20,
-              left: 0,
-              right: 0,
-              child: Container(
-                width: double.infinity * 1,
-                height: 60,
-                margin: const EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                    color: whiteC,
-                    borderRadius: BorderRadius.circular(50),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: greyC_400,
-                        blurRadius: 12,
-                        offset: Offset(0, 0),
-                      )
-                    ]),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                  child: Center(
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: itemsButtom.length,
-                      physics: const NeverScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return InkWell(
-                          onTap: () {
-                            setState(() {
-                              myCurrentIndex = index;
-                            });
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 25),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  itemsButtom[index],
-                                  size: 25,
-                                  color: myCurrentIndex == index
-                                      ? primaryC
-                                      : greyC_800,
-                                ),
-                                myCurrentIndex == index
-                                    ? spaceHeight(3)
-                                    : Container(),
-                                AnimatedContainer(
-                                  duration: const Duration(
-                                      milliseconds: 300), // Durasi animasi
-                                  curve: Curves.easeInOut, // Kurva animasi
-                                  width: myCurrentIndex == index
-                                      ? 20
-                                      : 15, // Ubah ukuran saat aktif
-                                  height: myCurrentIndex == index
-                                      ? 8
-                                      : 5, // Ubah ukuran saat aktif
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: myCurrentIndex == index
-                                        ? primaryC
-                                        : whiteC,
-                                  ),
-                                ),
-                              ],
-                            ),
+      extendBody: true,
+      body: body[myCurrentIndex],
+      bottomNavigationBar: Container(
+        width: double.infinity * 1,
+        height: 60,
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        decoration: BoxDecoration(
+            color: whiteC,
+            borderRadius: BorderRadius.circular(50),
+            boxShadow: const [
+              BoxShadow(
+                color: greyC_400,
+                blurRadius: 12,
+                offset: Offset(0, 0),
+              )
+            ]),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Center(
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: itemsButtom.length,
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    setState(() {
+                      myCurrentIndex = index;
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          itemsButtom[index],
+                          size: 25,
+                          color: myCurrentIndex == index ? primaryC : greyC_800,
+                        ),
+                        myCurrentIndex == index ? spaceHeight(3) : Container(),
+                        AnimatedContainer(
+                          duration: const Duration(
+                              milliseconds: 300), // Durasi animasi
+                          curve: Curves.easeInOut, // Kurva animasi
+                          width: myCurrentIndex == index
+                              ? 20
+                              : 15, // Ubah ukuran saat aktif
+                          height: myCurrentIndex == index
+                              ? 8
+                              : 5, // Ubah ukuran saat aktif
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: myCurrentIndex == index ? primaryC : whiteC,
                           ),
-                        );
-                      },
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              ),
+                );
+              },
             ),
-          ],
+          ),
         ),
       ),
-      // bottomNavigationBar:
     );
   }
 }
